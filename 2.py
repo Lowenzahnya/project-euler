b5892c9c0eb40f72dae166b9c075e2e26edc5d84
Lowenzahnya <lowenzahnya@yandex.ru>
2023-04-23 11:19:09 +0300

    !1

---
+++ 2.py
@@ -0,0 +1,37 @@
+def fibonacci(n):  # Числа Фибоначчи в заданном диапазоне
+    result = [1, 2]
+    f1 = f2 = 1
+    for i in range(2, n):
+        f1, f2 = f2, f1 + f2
+        i = f1 + f2
+        result += [i]
+    return result
+
+
+def sum_even(result):  # Сумма четных чисел из диапазона
+    evensum = []
+    for k in range(len(result)):
+        if result[k] % 2 == 0:  # and result[k] < 4000000 (если по условию задачи)
+            evensum.append(result[k])
+    return sum(evensum)
+# Функцию можно также представить в виде
+# evensum = [res[k] for k in range(len(res)) if res[k] % 2 == 0]
+
+
+n = int(input('Введите количество чисел: '))
+print(fibonacci(n))
+print(sum_even(fibonacci(n)))
+kl = list(map(int, input('Введите числа через пробел').strip().split()))
+print(sum_even(kl))  # Сумма произвольных чисел
+
+""" Решение задачи простым способом:
+f1 = f2 = 1
+fib = 0
+result = 0
+while fib < 4000000:
+    fib = f1 + f2
+    if fib % 2 == 0:
+        result += fib
+    f1, f2 = f2, fib
+print(result)
+"""
