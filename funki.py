71de0e3cf450b308bbcb1bdc61330f6677bc7bf3
Oleg <lowenzahnya@yandex.ru>
2023-05-11 15:26:13 +0300

    Update funki.py

--- funki.py
+++ funki.py
@@ -1,14 +1,11 @@
-"""
-Длина отрезка в системе координат
+# Длина отрезка в системе координат
 def lensec():
     return ((x2-x1)**2 + (y2-y1)**2)**0.5
 
 x1, y1, x2, y2 = [float(n) for n in input('координаты').split()]
 print(lensec())
-"""
 
-"""
-Рекурсия обратного вывода (1 2 3 -> 3 2 1), Стек данных
+# Рекурсия обратного вывода (1 2 3 -> 3 2 1), Стек данных
 def reverse():
     n = int(input('Neponyatna'))
     if n != 0:
@@ -16,16 +13,12 @@ def reverse():
     print(n)
 
 reverse()
-"""
 
-"""
-Заполнение двумерного массива циферками
+# Заполнение двумерного массива циферками
 m = [[int(j) for j in input('j').split()] for i in range(int(input('n')))]
 print(m)
-"""
 
-"""
-Матрица из 0 4х4
+# Матрица из 0 4х4
 a = [[0]*4 for i in range(4)]
 for row in a:
     print(' '.join([str(elem) for elem in row]))
@@ -42,10 +35,8 @@ for row in a:
 
 for row in [[j*0 for j in range(6)] for i in range(5)]:
     print(' '.join([str(elem) for elem in row]))
-"""
 
-"""
-Шахматная доска
+# Шахматная доска
 n = int(input('dlina'))
 m = int(input('visota'))
 k = [['.']*n for i in range(m)]
@@ -55,10 +46,9 @@ for i in range(m):
             k[i][j] = '*'
 for row in k:
     print(' '.join(row))
-"""
 
-# print(len(set(input('S').split()))) # Количество уникальных элементов
-# print(len(set(input('S').split()) & set(input('K').split()))) # Количество совпадений множеств
-# print(set(input('S').split()) - set(input('K').split()))
-# print(set(input('S').split() + input('K').split()))
-# print(*sorted(set(input('S').split()) & set(input('K').split())))
+print(len(set(input('S').split()))) # Количество уникальных элементов
+print(len(set(input('S').split()) & set(input('K').split()))) # Количество совпадений множеств
+print(set(input('S').split()) - set(input('K').split()))
+print(set(input('S').split() + input('K').split()))
+print(*sorted(set(input('S').split()) & set(input('K').split())))
